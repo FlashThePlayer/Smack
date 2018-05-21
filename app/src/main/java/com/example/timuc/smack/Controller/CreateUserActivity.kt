3555cf9fd0c66152421af7f84c701c7380a503a8
Timucin Acar <timucins_map@hotmail.de>
2018-05-22 00:50:56 +0200

    lesson 6 commit

--- app/src/main/java/com/example/timuc/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/timuc/smack/Controller/CreateUserActivity.kt
@@ -12,49 +12,56 @@ import java.util.*
 class CreateUserActivity : AppCompatActivity() {
 
     var userAvatar = "profileDefault"
-    var avatarColor ="[0.5, 0.5, 0.5, 1]"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
     }
 
-    fun generateUserAvatar(view: View){
+    fun generateUserAvatar(view: View) {
         val random = Random()
         val color = random.nextInt(2)
         val avatar = random.nextInt(28)
 
-        if (color == 0){
+        if (color == 0) {
             userAvatar = "light$avatar"
         } else {
-            userAvatar ="dark$avatar"
+            userAvatar = "dark$avatar"
         }
 
         val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
         createAvatarImageView.setImageResource(resourceId)
     }
 
-    fun createUserClicked(view: View){
-        AuthService.registerUser(this, "t@a.com", "123456" ){complete ->
-            if (complete){
+    fun createUserClicked(view: View) {
+        val email = createEmailTxt.text.toString()
+        val password = createPasswordTxt.text.toString()
+        AuthService.registerUser(this, email, password) { registerSuccess ->
+            if (registerSuccess) {
+                AuthService.loginUser(this, email, password) { loginSuccess ->
+                    if (loginSuccess) {
+                        println(AuthService.userEmail)
+                        println(AuthService.authToken)
+                    }
+                }
 
             }
-
         }
     }
 
-    fun generateColorClicked(view: View){
-        val random =  Random()
-        val r = random.nextInt(255)
-        val g = random.nextInt(255)
-        val b = random.nextInt(255)
+        fun generateColorClicked(view: View) {
+            val random = Random()
+            val r = random.nextInt(255)
+            val g = random.nextInt(255)
+            val b = random.nextInt(255)
 
-        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+            createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
 
-        val savedR = r.toDouble()/255
-        val savedG = g.toDouble()/255
-        val savedB = b.toDouble()/255
+            val savedR = r.toDouble() / 255
+            val savedG = g.toDouble() / 255
+            val savedB = b.toDouble() / 255
 
-        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+            avatarColor = "[$savedR, $savedG, $savedB, 1]"
+        }
     }
-}
